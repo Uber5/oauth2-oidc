74cc8f5cee5c8a8dad6c5a4fa883b568f1e4368d
Chris Oloff <christian.oloff@gmail.com>
2015-11-05 10:57:46 +0200

    dummy specs (with some logic), now functional

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ var OAuth2OIDC = function(options) {
   this.options = options || {};
 }
 
+function validateAuth(req, res, next) {
+  console.log('validate...');
+  if (req.params.x1) {
+    return next();
+  } else {
+    return next('expected x1 param');
+  }
+}
+OAuth2OIDC.prototype._validateAuth = validateAuth;
+
+function performAuth(req, res, next) {
+  console.log('do it');
+  next('oops');
+}
+OAuth2OIDC.prototype._performAuth = performAuth;
+
 OAuth2OIDC.prototype.auth = function() {
-  return [ function(req, res, next) {
-    console.log('validate...');
-    next();
-  }, function(req, res, next) {
-    console.log('do it');
-    next();
-  }];
+  return [ validateAuth, performAuth ];
 };
 module.exports = OAuth2OIDC;
--- spec/authSpec.js
+++ spec/authSpec.js
@@ -1,6 +1,36 @@
+var httpMocks = require('node-mocks-http');
+
 describe('auth', function() {
-  it('succeeds if query param x1 given (DUMMY)', function() {
+
+  var OAuth2OIDC = require('../index');
+  var oidc = new OAuth2OIDC({});
+
+  it('succeeds if query param x1 given (DUMMY)', function(done) {
+    var req = httpMocks.createRequest({
+      method: 'GET',
+        url: '/whatever',
+        params: {
+          x1: 3
+        }
+    });
+    var res = httpMocks.createResponse();
+    oidc._validateAuth(req, res, function(err) {
+      expect(err).toBe(undefined);
+      done();
+    });
   });
-  it('fails if no param given', function() {
+  it('fails if no param given', function(done) {
+    var req = httpMocks.createRequest({
+      method: 'GET',
+        url: '/whatever',
+        params: {
+          x2: 3
+        }
+    });
+    var res = httpMocks.createResponse();
+    oidc._validateAuth(req, res, function(err) {
+      expect(err).toEqual('expected x1 param');
+      done();
+    });
   });
 });
