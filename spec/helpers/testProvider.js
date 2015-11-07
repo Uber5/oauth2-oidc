23247c02d626514202ca815152d08103c5472d77
Chris Oloff <christian.oloff@gmail.com>
2015-11-07 16:52:38 +0200

    TestProvider, added express-session

--- spec/helpers/testProvider.js
+++ spec/helpers/testProvider.js
@@ -3,12 +3,17 @@
 const express = require('express'),
       OAuth2OIDC = require('../..'),
       validate = require('jsonschema').validate,
-      debug = require('debug')('oauth2-oidc')
+      debug = require('debug')('oauth2-oidc'),
+      session = require('express-session'),
+      crypto = require('crypto')
 
 class TestProvider {
   constructor(config) {
     const app = express()
     const oauth2oidc = new OAuth2OIDC(config)
+    app.use(session({
+      secret: crypto.randomBytes(12).toString('base64')
+    }))
     app.use('/user/authorize', oauth2oidc.auth())
     this._app = app
   }
