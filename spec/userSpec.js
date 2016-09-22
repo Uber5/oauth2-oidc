af154cf7d2783025f522096bdadd0d33d8f360a5
Chris Oloff <christian.oloff@gmail.com>
2016-09-22 12:06:43 +0200

    fix: do not crash when user.findOne() does not find anything

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -3,15 +3,15 @@
 const debug = require('debug')('oauth2-oidc')
 
 describe('user', function() {
-  describe('given a user', function() {
-    let config
-    beforeEach(function(done) {
-      getState().then(state => {
-        const oidc = new OAuth2OIDC({ state, login_url: '/xyz' })
-        config = oidc.options
-        done()
-      })
+  let config
+  beforeEach(function(done) {
+    getState().then(state => {
+      const oidc = new OAuth2OIDC({ state, login_url: '/xyz' })
+      config = oidc.options
+      done()
     })
+  })
+  describe('given a user', function() {
     it('allows me to update the password', function(done) {
       Promise.resolve(buildAndSaveUser(config.state.collections, {
         password: 'secret887',
@@ -26,6 +26,19 @@ describe('user', function() {
       }).catch((err) => {
         debug('updating password, err', err)
         throw new Error(err)
+      })
+    })
+  })
+  describe('finding', function() {
+    it('is empty when user not found', done => {
+      config.state.collections.user.findOne({ thisQuery: 'should return nothing' })
+      .then(user => {
+        expect(user).toBeFalsy()
+        done()
+      })
+      .catch(err => {
+        console.log('testing for empty', err)
+        throw err
       })
     })
   })
